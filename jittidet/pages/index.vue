01c3f273e0184cee500bde337ebdfd95afe5dd47
SatorJP <75246985+mommore@users.noreply.github.com>
2024-08-22 10:15:09 +0700

    set-up and template

---
+++ jittidet/pages/index.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="12" class="mt-15">
+      <h1>ABOUT ME</h1>
+    </v-col>
+    <v-col cols="12" sm="8" md="12">
+      <p>Stack</p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, adipisci
+        voluptates aliquid quas sapiente excepturi libero sequi neque recusandae
+        repellat error obcaecati odit inventore alias a provident natus placeat
+        dignissimos.
+      </p>
+    </v-col>
+    <v-col cols="12"><hr /></v-col>
+    <v-col cols="12" sm="8" md="12" class="mt-5">
+      <h1>EXPERIENCE</h1>
+    </v-col>
+    <v-col cols="12">
+      <h2>Biofarm App</h2>
+    </v-col>
+    <v-col cols="12">
+      <v-img
+        widht = "300"
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Fimage_1160358&psig=AOvVaw20ZKvh1r06SpvOohPf0Lhp&ust=1724380997544000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMC1rfjJh4gDFQAAAAAdAAAAABAY"
+      ></v-img>
+    </v-col>
+    <v-col cols="12" sm="8" md="12">
+      <p>Stack</p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, adipisci
+        voluptates aliquid quas sapiente excepturi libero sequi neque recusandae
+        repellat error obcaecati odit inventore alias a provident natus placeat
+        dignissimos.
+      </p>
+    </v-col>
+    <v-col cols="12" class="mt-5">
+      <h2>News Blogs</h2>
+    </v-col>
+    <v-col cols="12">
+      <v-img
+        widht = "300"
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Fimage_1160358&psig=AOvVaw20ZKvh1r06SpvOohPf0Lhp&ust=1724380997544000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMC1rfjJh4gDFQAAAAAdAAAAABAY"
+      ></v-img>
+    </v-col>
+    <v-col cols="12" sm="8" md="12">
+      <p>Stack</p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, adipisci
+        voluptates aliquid quas sapiente excepturi libero sequi neque recusandae
+        repellat error obcaecati odit inventore alias a provident natus placeat
+        dignissimos.
+      </p>
+    </v-col>
+    <v-col cols="12" class="mt-5">
+      <h2>Food Processing Pilot Plant</h2>
+    </v-col>
+    <v-col cols="12">
+      <v-img
+        widht = "300"
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Fimage_1160358&psig=AOvVaw20ZKvh1r06SpvOohPf0Lhp&ust=1724380997544000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMC1rfjJh4gDFQAAAAAdAAAAABAY"
+      ></v-img>
+    </v-col>
+    <v-col cols="12" sm="8" md="12">
+      <p>Stack</p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, adipisci
+        voluptates aliquid quas sapiente excepturi libero sequi neque recusandae
+        repellat error obcaecati odit inventore alias a provident natus placeat
+        dignissimos.
+      </p>
+    </v-col>
+    <v-col cols="12"><hr></v-col>
+    <v-col cols="12" class="mt-5">
+      <h1>CONTACT</h1>
+    </v-col>
+    <v-col cols="12" sm="8" md="12">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, adipisci
+        voluptates aliquid quas sapiente excepturi libero sequi neque recusandae
+        repellat error obcaecati odit inventore alias a provident natus placeat
+        dignissimos.
+      </p>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
